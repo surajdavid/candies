8fe1970e943472f0bae4d224a3bd0aa9ead61bc2
suraj <suraj.demotest@gmail.com>
2024-08-12 18:10:51 +0530

    This is experiment empty commit. I will add classes later.

---
+++ Candies/src/main/java/com/myblog/CandiesApplication.java
@@ -0,0 +1,13 @@
+package com.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandiesApplication.class, args);
+	}
+
+}
